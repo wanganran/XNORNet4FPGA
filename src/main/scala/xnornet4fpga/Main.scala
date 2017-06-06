20b97f781884d21a4054f07d2fcab414a9af6967
wanganran <wanganran01@gmail.com>
2017-06-05 22:41:32 -0700

    for reduced resource

--- src/main/scala/xnornet4fpga/Main.scala
+++ src/main/scala/xnornet4fpga/Main.scala
@@ -6,7 +6,7 @@ import chisel3.iotesters.PeekPokeTester
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val generator=()=>new IglooScheduler(HardwareConfig.default, SimpleMLP(256, 10, 96))
+    val generator=()=>new IglooScheduler(HardwareConfig.twoway, SimpleMLP(256, 10, 96))
     chisel3.Driver.execute(args, generator)
   }
 }
